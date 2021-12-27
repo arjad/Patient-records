fcbdac6179bb585bcb80cea480d5ebc169e6ca56
arjad <arjadgohar14@gmail.com>
2021-12-27 14:36:09 +0500

    hospital search record system online

--- src/App.js
+++ src/App.js
@@ -4,23 +4,52 @@ import data from "./MOCK_DATA.json";
 
 function App() 
 {
+  const [search, setsearch] = useState("")
   return (
-  <div className="App">
+  <div className="App bg-dark p-5 ">
   <br/>
-    app
-    <input placeholder="search nay patient name"/>
+    <h3 className="text-white">SEARCH BY NAME,GENDER, EMAIL, PLACE</h3>
+    <input className="form-control" placeholder="search by patient name" onChange={(e)=>setsearch(e.target.value)}/>
   <br/>
 
-    <div className="container app bg-light">
-      {data.map((val,key)=>{
+    <div className="container app ">
+      {data.filter((val)=>{
+        if(search == "")
+        {
+          //return every item in array
+          return val;
+        }
+        else if(val.first_name.toLowerCase().includes(search.toLowerCase()))
+        {
+          return val;
+        }
+        else if(val.last_name.toLowerCase().includes(search.toLowerCase()))
+        {
+          return val;
+        }
+        else if(val.email.includes(search.toLowerCase()))
+        {
+          return val;
+        }else if(val.blood_group.toLowerCase().includes(search.toLowerCase()))
+        {
+          return val;
+        }else if(val.gender.toLowerCase().includes(search.toLowerCase()))
+        {
+          return val;
+        }
+        
+       
+      }).map((val,key)=>{
         return <div class="card">
-        <img src={val.dp} alt="image" style={{width:"100%"}}/>
-        <h3>{val.first_name}{val.last_name}<span class="badge badge-secondary">{val.blood_group}</span>         </h3>
+        <img src={val.dp} alt="wrong URL" style={{width:"100%"}}/>
+        <h3>
+          {val.first_name}{val.last_name}
+          <span class="badge badge-secondary">{val.blood_group}</span>         
+        </h3>
         ({val.gender})
         <p class="title">{val.phone_no}</p>
         <p>{val.email}</p>
         <p>{val.from}</p>
-        <p>Harvard University</p>
         <div style={{margin: `24px 0`}}>
           <a href="#"><i class="fa fa-dribbble"></i></a> 
           <a href="#"><i class="fa fa-twitter"></i></a>  
